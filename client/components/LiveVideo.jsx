2656623f9b6af130f8c49f43358053ade5cc2f72
sohamingle <sohamingle@gmail.com>
2023-09-28 16:30:14 +0530

    new

--- client/components/LiveVideo.jsx
+++ client/components/LiveVideo.jsx
@@ -19,19 +19,14 @@ const LiveVideo = ({videoUrl}) => {
   return (
     <div className='relative'>
         {textOverlays.map((text)=>(
-          <>
           <div key={text.name} className='absolute z-10 text-white' style={{bottom:`${text.position_y}%`,left:`${text.position_x}%`,width:`${text.width}%`,height:`${text.height}%`}}>
               {text.value}
           </div>
-          </>
         ))}
         {imageOverlays.map((image)=>(
-          <>
-          
           <div key={image.name} className='absolute z-10 text-white' style={{bottom:`${image.position_y}%`,left:`${image.position_x}%`,width:`${image.width}%`,height:`${image.height}%`}}>
               <img src={image.value} alt={image.name}/>
           </div>
-          </>
         ))}
         <iframe className='z-0 md:w-[640px] md:h-[480px]'  src={videoUrl}  allowFullScreen >
         </iframe>
